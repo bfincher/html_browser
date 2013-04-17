717af20e6579692954264e7258010af2064d1dbd
bfincher <devnull@localhost>
2013-04-17 09:52:03 -0500

    change database host

--- html_browser_site/settings.py
+++ html_browser_site/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'NAME': 'hb',                      # Or path to database file if using sqlite3.
         'USER': 'hb',                      # Not used with sqlite3.
         'PASSWORD': 'hb',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'DB_HOST',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
