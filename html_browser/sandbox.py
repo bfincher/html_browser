28bb5c9588b84e69c90b5975535334f80168771e
Brian Fincher <brian@fincherhome.com>
2013-07-28 16:51:56 -0500

    add a sandbox file to write test/prototype code

---
+++ html_browser/sandbox.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.insert(0, '/srv/www/hb')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'html_browser_site.settings'
+from django.contrib.auth.models import User, Group
+
+
+user = User.objects.all()[1]
+group = Group.objects.all()[1]
+#val = group in user.groups
