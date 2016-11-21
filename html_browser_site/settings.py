182e39e0cbc508eea50f960fa58f7f0fd977a5aa
Brian Fincher <brian@fincherhome.com>
2016-11-21 10:40:16 -0600

    remove deprecated TEMPLATE_ settings

--- html_browser_site/settings.py
+++ html_browser_site/settings.py
@@ -6,7 +6,6 @@ URL_PREFIX = r''
 SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(os.path.join('../', __file__))))
 
@@ -114,13 +113,6 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'm9pu5&amp;-q3&amp;@x!d9ldr9vc1tb7qgj-i*ygui%snx0azfew%r39a'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -153,6 +145,7 @@ TEMPLATES = [
                 'html_browser.context_processors.media',
                 'html_browser.context_processors.images',
             ],
+            'debug': True,
         },
     },
 ]
