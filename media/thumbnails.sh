6a5d18019c94b0655f336314568e71d7b1b623b1
Brian Fincher <brian@fincherhome.com>
2016-12-08 02:47:51 -0600

    delete obsolete thumbnail scripts

--- media/thumbnails.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#java -version
-#java -jar /usr/local/tomcat/thumbnail.jar /mnt/nfs/Pictures /usr/local/tomcat/webapps/thumbs > /usr/local/tomcat/logs/thumbnail.log
-
-python thumbnail.py /mnt/nfs/Pictures /srv/www/hb/media/thumbs > /var/log/thumbnail.log 2>&1
-
