175c4174b4bfb578de27cc9f41b6155d9cdc644e
Brian Fincher <brian@fincherhome.com>
2019-06-17 14:19:26 -0500

    wip on travis docker

--- docker/run_docker_container.sh
+++ docker/run_docker_container.sh
@@ -1,8 +1,24 @@
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+UID=1000
+GID=1000
+USERNAME=$(getent passwd $UID | gawk -F':' '{ print $1}')
+GROUPNAME=$(getent group $GID | gawk -F':' '{ print $1}')
+
 if [ -z $VIRTUAL_ENV ]; then
     image_name=html_browser_web
+    CONFIG=${THIS_DIR}/config
 else
     image_name=$(basename $VIRTUAL_ENV)
+    CONFIG=${THIS_DIR}/config_$image_name
 fi
+
+if [ ! -d $CONFIG ]; then
+    mkdir $CONFIG
+fi
+
+chown -R ${USERNAME}:${GROUPNAME} $CONFIG
+
 
 if [ $# -eq 1 ]; then
     port=$1
@@ -11,11 +27,11 @@ else
 fi
 
 docker run -d \
-    -p ${port}:8000 \
-    -v /Volumes/data1/code/workspace/html_browser/config:/config \
+    -p $port:80 \
+    -v ${CONFIG}:/config \
     -v /Volumes/data1:/data1 \
-    -e APP_UID=1009 \
-    -e APP_GID=1009 \
+    -e APP_UID=$UID \
+    -e APP_GID=$GID \
     --name=$image_name \
     --restart unless-stopped \
     $image_name
