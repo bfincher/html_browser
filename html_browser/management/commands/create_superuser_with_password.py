785d056f20de602a8dbf338d9599d7e238ca18e9
Brian Fincher <brian@fincherhome.com>
2019-06-25 21:53:34 -0500

    create management command to create superuser
    (cherry picked from commit 0b8a5b43fa11adfbf25e3726df589b10a42aa607)

---
+++ html_browser/management/commands/create_superuser_with_password.py
@@ -0,0 +1,47 @@
+from annoying.functions import get_object_or_None
+from django.contrib.auth.management.commands import createsuperuser
+from django.core.management import CommandError
+
+
+class Command(createsuperuser.Command):
+    help = 'Crate a superuser, and allow password to be provided'
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            '--password', dest='password', default=None,
+            help='Specifies the password for the superuser.',
+        )
+        parser.add_argument(
+            '--preserve', dest='preserve', default=False, action='store_true',
+            help='Exit normally if the user already exists.',
+        ),
+
+        parser.add_argument(
+            '--if-no-superuser', '--if-no-superuser', action='store_false', dest='if_no_superuser',
+            help=('Will only create the specified user if there is a super user does not currently exist'))
+
+    def handle(self, *args, **options):
+        password = options.get('password')
+        username = options.get('username')
+        database = options.get('database')
+
+        if options['if_no_superuser'] and get_object_or_None(User, is_superuser=True):
+            self.stdout.write('Not creating super user since one already exists')
+            return
+        if password and not username:
+            raise CommandError("--username is required if specifying --password")
+
+        if username and options.get('preserve'):
+            exists = self.UserModel._default_manager.db_manager(database).filter(username=username).exists()
+            if exists:
+                self.stdout.write("User exists, exiting normally due to --preserve")
+                return
+
+        super(Command, self).handle(*args, **options)
+
+        if password:
+            user = self.UserModel._default_manager.db_manager(database).get(username=username)
+            user.set_password(password)
+            user.save()
+
